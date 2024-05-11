471642edd848c83c02d9062d941ba8107ed6ba47
asasmith <smith.asa.la@gmail.com>
2024-05-11 10:02:00 -0400

    feat: Add courses and monthly-cheats pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
             fontFamily: {
                 bodyFont: ['"Galano Grotesque"', 'sans-serif'],
                 headingFont: ['"Quincy CF"', 'sans-serif'],
+            },
+            colors: {
+                black: '#1d1d1d',
+                white: '#FAF8F6',
             }
         },
     },
